0c5f0358e43374c67f928f4ccd5102cb592b3098
FlyingRatMan <olya.shalashova@gmail.com>
2022-02-18 21:01:32 +0100

    cw2+star task, fixed hw2

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ app.set('views', path.join(__dirname, 'static'));
 const users = [];
 let filteredUsers = [];
 
+app.get('/sign-in', ({body}, res) => {
+    res.render('sign-in');
+});
+
 app.get('/login', (req, res) => {
     res.render('login');
 });
@@ -25,21 +29,26 @@ app.get('/users', (req, res) => {
     if (req.query.city) {
         filteredUsers = (users.filter(user => user.city === req.query.city));
         res.render('users', {users: filteredUsers});
+        return;
     }
     if (req.query.age) {
         filteredUsers = (users.filter(user => user.age === req.query.age));
         res.render('users', {users: filteredUsers});
+        return;
     }
 
     res.render('users', {users});
 });
 
-app.get('/users/:id', (req, res) => {
-    const {id} = req.params;
-    const user = users[id - 1];
-    res.render('user', {user});
+app.get('/users/:id', ({params}, res) => {
+    const {id} = params;
+    let user = users.find(user => user.id === +id);
+    res.render('user', {user: user});
 });
 
+app.get('/error', (req, res) => {
+    res.render('error');
+});
 
 app.post('/login', ({body}, res) => {
     const userExist = users.some(user => user.email === body.email);
@@ -47,9 +56,25 @@ app.post('/login', ({body}, res) => {
         res.redirect('/error');
         return;
     }
-    users.push(body);
+
+    users.push({...body, id: users.length? users.length + 1 : 1});
     res.redirect('/users');
-    // users.filter(user => user.email === body.email).length ? res.redirect('/error') : users.push(body);
+});
+
+app.post('/sign-in', ({body, params}, res) => {
+    const signedUser = users.find(user => user.email === body.email && user.password === body.password);
+    if (signedUser) {
+        const id = signedUser.id;
+        res.redirect(`/users/${id}`);
+    }
+});
+
+app.post('/users/:id', ({body, query}, res) => {
+    if (query.method === 'delete') {
+        users.splice(users[body.id], 1);
+    }
+
+    res.redirect('/users');
 });
 
 app.use((req, res) => {
